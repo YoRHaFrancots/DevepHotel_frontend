3be8dec5e3ee7d4a123481c6dcee53b275eb3029
LauraGL <laugranadolopez@gmail.com>
2023-11-04 20:20:54 +0100

    Agrego a main Nabvar y Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+
+const Footer = () => {
+  return (
+    <footer className="fixed-bottom bg-light ">
+      <div className="container text-center mb-2">
+        <div className="social-icons">
+          <a href="enlaceFacebook" className="logos_redes">
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
+          <a href="enlaceTwiter" className="logos_redes">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+          <a href="enlaceInstagram" className="logos_redes">
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import logoDevepHotel from "../assets/imgs/logoDevepHotel.png";
+import "../css/index.css";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img
+            className="logo-DevepHotelNavbar"
+            src={logoDevepHotel}
+            alt="logo hotel"
+          />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Inicio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                El hotel
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link " aria-current="page" href="#">
+                Conócenos
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link " aria-current="page" href="#">
+                Galería
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link " aria-current="page" href="#">
+                Contacto
+              </a>
+            </li>
+          </ul>
+        </div>
+        <button>Inicio Sesión</button>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
