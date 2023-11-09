f3060fb19bb4348b99ce3b2a08c16fb17ff8e4e5
LauraGL <laugranadolopez@gmail.com>
2023-11-09 20:26:55 +0100

    Importación de Route pagina404

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import "./App.css";
 import Footer from "./components/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeScreen from "./pages/HomeScreen";
-import AboutScreen from "./pages/AboutScreen"
-import ContactScreen from "./pages/ContactScreen"
-import GalleryScreen from "./pages/GalleryScreen"
-import LoginScreen from "./pages/LoginScreen"
-import HotelScreen from "./pages/HotelScreen"
+import AboutScreen from "./pages/AboutScreen";
+import ContactScreen from "./pages/ContactScreen";
+import GalleryScreen from "./pages/GalleryScreen";
+import LoginScreen from "./pages/LoginScreen";
+import HotelScreen from "./pages/HotelScreen";
+import Error404 from "./components/Error404";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path="/gallery" element={<GalleryScreen />} />
           <Route path="/login" element={<LoginScreen />} />
           <Route path="/hotel" element={<HotelScreen />} />
+          <Route path="*" element={<Error404 />} />
         </Routes>
         <Footer />
       </BrowserRouter>
