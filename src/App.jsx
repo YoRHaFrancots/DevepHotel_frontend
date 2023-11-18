7d2040c6eb613f169665c4087b9371c34b5c7027
LauraGL <laugranadolopez@gmail.com>
2023-11-18 12:54:43 +0100

    Elimino enlaces no usados (imagen y about)

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,6 @@ import ContactScreen from "./pages/ContactScreen";
 import GalleryScreen from "./pages/GalleryScreen";
 import LoginScreen from "./pages/LoginScreen";
 import HotelScreen from "./pages/HotelScreen";
-import RegScreen from "./pages/RegScreen";
-
-// import AboutScreen from "./pages/AboutScreen";
-
-import Error404 from "./components/Error404";
-
 function App() {
   const [modoOscuro, setModoOscuro] = useState(false);
 
@@ -31,16 +25,15 @@ function App() {
               <HomeScreen modoOscuro={modoOscuro} cambiarModo={cambiarModo} />
             }
           />
-
-          {/* <Route path="/about" element={<AboutScreen />} /> */}
-
           <Route path="/contact" element={<ContactScreen />} />
           <Route path="/gallery" element={<GalleryScreen />} />
           <Route path="/login" element={<LoginScreen />} />
-          <Route path="/hotel" element={<HotelScreen />} />
-          <Route path="/register" element={<RegScreen />} />
-
-          <Route path="*" element={<Error404 />} />
+          <Route
+            path="/hotel"
+            element={
+              <HotelScreen modoOscuro={modoOscuro} cambiarModo={cambiarModo} />
+            }
+          />
         </Routes>
         <Footer modoOscuro={modoOscuro} cambiarModo={cambiarModo} />
       </BrowserRouter>
