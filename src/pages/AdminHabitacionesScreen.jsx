0bfd73c6faf20504ed9e99d1a8754bfc326d095a
Franco Garcia <garciafranco_01@live.com>
2023-11-29 18:12:59 -0300

    Esqueleto habitaciones admin page

--- src/pages/AdminHabitacionesScreen.jsx
+++ src/pages/AdminHabitacionesScreen.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import {habitacionesList} from "../api/habitacionesApi"
 
 const AdminHabitacionesScreen = () => {
-  return <div>AdminHabitacionesScreen</div>;
+  useEffect(() => {
+    console.log("fetching data...");
+    fetchData();
+  }, []);
+  const [habitaciones, setHabitaciones] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const fetchData = async () => {
+    
+      const response = await habitacionesList();
+      console.log("Response from getUsersWithoutStatus:", response);
+      setHabitaciones(response.habitaciones);
+      setLoading(false);
+  
+  };
+  return (
+    <>
+      <br />
+      <br />
+      <br />
+      {loading == true ? (
+        <>
+          <div className="spinner-border custom-spinner" role="state">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+        </>
+      ) : (<div className="">test</div>)}
+    </>
+  );
 };
 
 export default AdminHabitacionesScreen;
