42c5a623e928cc125df6adab48c1ad36e87c7a8a
LauraGL <laugranadolopez@gmail.com>
2023-12-03 21:49:25 +0100

    Agrego modo oscuro a contact

--- src/pages/ContactScreen.jsx
+++ src/pages/ContactScreen.jsx
@@ -23,7 +23,13 @@ const ContactScreen = ({ modoOscuro, usuario }) => {
           <div className="col-12 mt-5  ">
             <div className="card mt-5 ">
               <div className="row ">
-                <div className="col-md-6 col-lg-6 d-none d-md-block img-contacto">
+                <div
+                  className={`${
+                    modoOscuro
+                      ? "col-md-6 col-lg-6 d-none d-md-block img-contacto bg-dark"
+                      : "col-md-6 col-lg-6 d-none d-md-block img-contacto"
+                  }`}
+                >
                   <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23958.464298535688!2d-65.24650755272408!3d-26.793891297480503!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225d0476d30c83%3A0x8e6a44ca591ac8c0!2zQXZlbmlkYSBBbcOpcmljYSAmIFBlcsO6LCBTYW4gTWlndWVsIGRlIFR1Y3Vtw6FuLCBUdWN1bcOhbg!5e0!3m2!1ses!2sar!4v1686695523241!5m2!1ses!2sar"
                     width="100%"
@@ -32,7 +38,13 @@ const ContactScreen = ({ modoOscuro, usuario }) => {
                     className={`${modoOscuro ? "modo-oscuro-iframe" : ""}`}
                   ></iframe>
                 </div>
-                <div className="col-md-12 col-lg-6">
+                <div
+                  className={`${
+                    modoOscuro
+                      ? "col-md-6 col-lg-6  bg-dark text-white"
+                      : "col-md-6 col-lg-6  img-contacto"
+                  }`}
+                >
                   <div className="card-body">
                     <h1>Mándanos tu consulta o comentario</h1>
                     <p>Déjanos tus datos y tus sugerencias sobre el hotel</p>
@@ -43,7 +55,11 @@ const ContactScreen = ({ modoOscuro, usuario }) => {
                         </label>
                         <input
                           type="text"
-                          className="form-control"
+                          className={` ${
+                            modoOscuro
+                              ? "form-control bg-secondary"
+                              : "form-control"
+                          }`}
                           {...register("name", {
                             required: "Este campo es requerido",
                             pattern: {
@@ -65,7 +81,11 @@ const ContactScreen = ({ modoOscuro, usuario }) => {
                         </label>
                         <input
                           type="email"
-                          className="form-control"
+                          className={` ${
+                            modoOscuro
+                              ? "form-control bg-secondary"
+                              : "form-control"
+                          }`}
                           {...register("email", {
                             required: "Este campo es requerido",
                             pattern: {
@@ -84,7 +104,11 @@ const ContactScreen = ({ modoOscuro, usuario }) => {
                           Déjanos tus sugerencias
                         </label>
                         <textarea
-                          className="form-control"
+                          className={` ${
+                            modoOscuro
+                              ? "form-control bg-secondary"
+                              : "form-control"
+                          }`}
                           {...register("mensaje", {
                             required: "Este campo es requerido",
                             pattern: {
