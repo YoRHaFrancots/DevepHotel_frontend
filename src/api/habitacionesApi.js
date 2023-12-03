2df0c429eabeab8bf39cbd86b688a919503649a8
LauraGL <laugranadolopez@gmail.com>
2023-12-03 18:58:44 +0100

    Cambio de url a local para pruebas

--- src/api/habitacionesApi.js
+++ src/api/habitacionesApi.js
@@ -1,14 +1,15 @@
-const url = "https://backend-devephotel-dev-xfed.4.us-1.fl0.io/api/habitaciones"
+// const url = "https://backend-devephotel-dev-xfed.4.us-1.fl0.io/api/habitaciones"
+const url = "http://localhost:3000/api/habitaciones";
 
 export const habitacionesList = async () => {
-    const resp = await fetch(url,  {
-      method: "GET",
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    });
-  
-    const data = await resp.json();
-  
-    return data;
-  };
+  const resp = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+
+  const data = await resp.json();
+
+  return data;
+};
